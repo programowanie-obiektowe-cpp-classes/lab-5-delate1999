fcffe3afad72907f1f3d998c4a19747bd0477ba6
MK <whatever@nevermind.com>
2022-12-07 22:30:08 +0100

    Zaliczenie - V1.0

--- foo.hpp
+++ foo.hpp
@@ -8,5 +8,11 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     // Twoja implementacja tutaj
-    return {};
+    std::vector<char> wektor;
+    for(std::list<Human>::reverse_iterator it = people.rbegin(); it != people.rend(); ++it) {
+        it->birthday();
+        it->isMonster() ? wektor.push_back('n') : wektor.push_back('y');
+    }   
+
+    return wektor;
 }
